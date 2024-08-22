e4a758b6102d694dd20d1ba6f4ea0ac0ed719225
timur <eshimov_tk@mail.ru>
2024-08-23 00:22:57 +0300

    update all files

--- games/calc-engine.js
+++ games/calc-engine.js
@@ -2,28 +2,26 @@ import readlineSync from 'readline-sync';
 import { getName, getRandomNumber, startMassage, checkingAnswers } from '../src/index.js';
 
 export const startCalcGame = () => {
-    const gameName = 'brain-calc';
-    startMassage(gameName);
-    const userName = getName();
-    for (let i = 0; i < 3; i += 1) {
-
-        const randomNumber1 = getRandomNumber();
-        const randomNumber2 = getRandomNumber();
-        const operators = ['+', '-', '*'];
-        const randomOperator = operators[getRandomNumber(0, operators.length - 1)];
-        const question = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
-        const expression = eval(question);
-        const correctAnswer = String(expression);
-        console.log(`What is the result of the expression?\nQuestion: ${question}?`);
-        const userAnswer = readlineSync.question('Your answer: ');
-        const endingGame = checkingAnswers(userName, userAnswer, correctAnswer, i, question);
-        
-        if (endingGame === false) {
-
-            break
-
-        };
+  const gameName = 'brain-calc';
+  startMassage(gameName);
+  const userName = getName();
+  for (let i = 0; i < 3; i += 1) {
+    
+    const randomNumber1 = getRandomNumber();
+    const randomNumber2 = getRandomNumber();
+    const operators = ['+', '-', '*'];
+    const randomOperator = operators[getRandomNumber(0, operators.length - 1)];
+    const question = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
+    const expression = eval(question);
+    const correctAnswer = String(expression);
+    console.log(`What is the result of the expression?\nQuestion: ${question}?`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const endingGame = checkingAnswers(userName, userAnswer, correctAnswer, i, question);
+    
+    if (endingGame === false) {
+        break
     };
+  };
 
 };
 export default startCalcGame;
