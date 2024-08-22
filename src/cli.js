d83044376dedefd31d511ff21cb286104a8cca0c
timur <eshimov_tk@mail.ru>
2024-08-22 20:10:00 +0300

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 export const startBrainGames = () => {
     console.log('brain-games\nWelcome to the Brain Games!');
-    const name = readlineSync.question('Your answer: ');
+    const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
 };
