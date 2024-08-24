fb5d41960dfe97522b1361b1a69e7e1aae982a96
timur <eshimov_tk@mail.ru>
2024-08-24 12:29:37 +0300

    update all files

--- games/calc-engine.js
+++ games/calc-engine.js
@@ -13,8 +13,8 @@ export const startCalcGame = () => {
     const operators = ['+', '-', '*'];
     const randomOperator = operators[getRandomNumber(0, operators.length - 1)];
     const question = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
-    expression(randomOperator, randomNumber1, randomNumber2);
-    const correctAnswer = String(expression);
+    const expressionResult = expression(randomOperator, randomNumber1, randomNumber2);
+    const correctAnswer = String(expressionResult);
     console.log(`What is the result of the expression?\nQuestion: ${question}?`);
     const userAnswer = readlineSync.question('Your answer: ');
     const endingGame = checkingAnswers(userName, userAnswer, correctAnswer, i, question);
