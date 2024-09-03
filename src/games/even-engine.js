37d31bf367ed5e83860e389d6b8a2ae62ddd9b9d
timur <eshimov_tk@mail.ru>
2024-09-03 19:09:59 +0300

    reviews from 09/02/2024 19:40

--- src/games/even-engine.js
+++ src/games/even-engine.js
@@ -2,12 +2,12 @@ import startGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (number) => (number % 2 ? 'no' : 'yes');
+const isEven = (number) => number % 2 === 0;
 
 const getDataGame = () => {
   const randomNumber = getRandomNumber();
   const question = randomNumber;
-  const correctAnswer = isEven(randomNumber);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 export default () => startGames(description, getDataGame);
